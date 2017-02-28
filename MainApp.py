5a3c8b7a84add80c16980e7c5abd8df99f5df840
Bao <dobh@mail.uc.edu>
2017-02-27 23:17:21 -0500

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -12,9 +12,11 @@ def getDates():
     import csv
     f = open('daily.csv','rb')
     reader = csv.reader(f)
+    data = []
     for row in reader:
-        s += str(row)
-    return s
+        temp = {'DATE':row[0]}
+        data.append(temp)
+    return json.dumps(data)
 
 @app.route("/historical/<date>", methods=['GET', 'DELETE'])
 def getInfo():
@@ -33,8 +35,9 @@ def addData():
 @app.route("/forecast/<date>", methods=['GET'])
 def forecast():
     return "FORECAST DATA"
-    
+
+'''    
 @app.errorHandler(404)
 def not_found(error):
     return make_response(jsonify ( {'error': 'Bad Request'}),404)
-
+'''
