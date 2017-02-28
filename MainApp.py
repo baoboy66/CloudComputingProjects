f40081be414c56be28283e2fc0c4470f0e6b4ec0
dobh <dobh@mail.uc.edu>
2017-02-27 20:13:06 -0500

    Added data file and main app

---
+++ MainApp.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, CSV
+
+app = Flask(__name__)
+
+def loadData():
+    f = open('daily.csv')
+    csv.reader(f)
+    f.close()
+
+@app.route("/")
+def main():
+    return "Hello!"
+
+@app.route("/historical", methods=['GET'])
+def getDates():
+    return "Dates"
+
+@app.route("historical/<date>", methods=['GET'])
+def getInfo():
+    return "GET INFO";
+
+
+@app.route("/historical", methods=['POST'])
+def addData():
+    return "POST ADD DATA";
+
+@app.route("historical/<date>", methods=['DELETE'])
+def deleteData():
+    return "DELETE DATA"
+
+@app.route("forecast/<date>", methods=['GET'])
+def forecast():
+    return "FORECAST DATA"
